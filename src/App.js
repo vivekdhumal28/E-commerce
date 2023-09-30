da7d37c7e830a94da1b1f8df3dc32b0947356287
vivekdhumal28 <vivekdhumal.28@gmail.com>
2023-09-30 23:38:43 +0530

    Context Used

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ import ErrorPage from "./ErrorPage";
 import { GlobalStyle } from "./GlobalStyle";
 import { ThemeProvider } from "styled-components";
 import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 const App = () => {
   const theme = {
@@ -54,6 +55,7 @@ const App = () => {
             <Route path="/cart" element={<Cart />}></Route>
             <Route path="*" element={<ErrorPage />}></Route>
           </Routes>
+          <Footer />
         </Router>
       </ThemeProvider>
     </>
--- src/Contact.js
+++ src/Contact.js
@@ -1,7 +1,54 @@
 import styled from "styled-components";
 
 const Contact = () => {
-  return <Wrapper>Contact page</Wrapper>;
+  return (
+    <Wrapper>
+      <h2 className="common-heading">Contact Page</h2>
+
+      <iframe
+        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d120073.18649111116!2d75.30520145!3d19.870096249999996!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bdb9815a369bc63%3A0x712d538b29a2a73e!2sAurangabad%2C%20Maharashtra!5e0!3m2!1sen!2sin!4v1692685568764!5m2!1sen!2sin"
+        width="100%"
+        height="400"
+        style={{ border: 0 }}
+        allowFullScreen=""
+        loading="lazy"
+        referrerPolicy="no-referrer-when-downgrade"
+      ></iframe>
+
+      <div className="container">
+        <div className="contact-form">
+          <form action="" method="POST" className="contact-inputs">
+            <input
+              type="text"
+              placeholder="username"
+              name="username"
+              required
+              autoComplete="off"
+            />
+
+            <input
+              type="email"
+              placeholder="Email"
+              name="email"
+              required
+              autoComplete="off"
+            />
+
+            <textarea
+              name="message"
+              cols="30"
+              rows="10"
+              placeholder="Enter your message"
+              required
+              autoComplete="off"
+            ></textarea>
+
+            <input type="submit" value="send" />
+          </form>
+        </div>
+      </div>
+    </Wrapper>
+  );
 };
 
 const Wrapper = styled.section`
